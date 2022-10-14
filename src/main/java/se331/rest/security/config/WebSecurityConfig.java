2152a252f00058364f51edc4f2c1b0c10420cff6
whatsuplinlin <kjm010704@gmail.com>
2022-10-14 16:05:53 +0700

    7

--- src/main/java/se331/rest/security/config/WebSecurityConfig.java
+++ src/main/java/se331/rest/security/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .authorizeRequests()
-                .antMatchers("/auth/**",  "/refresh").permitAll()
+                .antMatchers("/auth/**",  "/refresh", "/register").permitAll()
                 .antMatchers(HttpMethod.GET,"/event").permitAll()
                 .antMatchers(HttpMethod.GET,"/organizers").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
--- src/main/java/se331/rest/security/controller/AuthenticationRestController.java
+++ src/main/java/se331/rest/security/controller/AuthenticationRestController.java
@@ -1,6 +1,7 @@
 package se331.rest.security.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +13,31 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import se331.rest.entity.Organizer;
+import se331.rest.repository.OrganizerRepository;
+import se331.rest.security.entity.Authority;
+import se331.rest.security.entity.AuthorityName;
 import se331.rest.security.entity.JwtUser;
 import se331.rest.security.entity.User;
+import se331.rest.security.repository.AuthorityRepository;
 import se331.rest.security.repository.UserRepository;
 import se331.rest.security.util.JwtTokenUtil;
 import se331.rest.util.LabMapper;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @CrossOrigin
+@Slf4j
 public class AuthenticationRestController {
 
     @Value("${jwt.header}")
@@ -42,9 +55,15 @@ public class AuthenticationRestController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    AuthorityRepository authorityRepository;
+
+    @Autowired
+    OrganizerRepository organizerRepository;
+
     @PostMapping("${jwt.route.authentication.path}")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException {
-
+        log.info("calling Authentication");
         // Perform the security
         final Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -53,7 +72,7 @@ public class AuthenticationRestController {
                 )
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
+        log.info("check if it reached?");
         // Reload password post-security so we can generate token
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String token = jwtTokenUtil.generateToken(userDetails);
@@ -82,5 +101,34 @@ public class AuthenticationRestController {
         }
     }
 
+    @PostMapping("/register")
+    @Transactional
+    public ResponseEntity<?> register(@RequestBody User request) {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        Authority authAdmin = Authority.builder().name(AuthorityName.ROLE_ADMIN).build();
+
+        authorityRepository.save(authAdmin);
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .firstname("")
+                .lastname("")
+                .username(request.getUsername())
+                .password(encoder.encode(request.getPassword()))
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021, 01, 01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        Organizer organizer = Organizer.builder()
+                .name(user.getFirstname() + " " + user.getLastname())
+                .build();
+
+        organizer.setUser(user);
+        user.setOrganizer(organizer);
+        organizerRepository.save(organizer);
+
+        user.getAuthorities().add(authAdmin);
+        userRepository.save(user);
+        return ResponseEntity.ok("Register success!!");
+    }
 
 }
