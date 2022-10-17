299028f20117a98056345426099ef828c0fa1947
whatsuplinlin <kjm010704@gmail.com>
2022-10-17 10:02:45 +0700

    8 submit

--- src/main/java/se331/rest/Lab08Application.java
+++ src/main/java/se331/rest/Lab08Application.java
@@ -19,7 +19,7 @@ public class Lab08Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://52.55.103.186:4000")
+                        .allowedOrigins("http://localhost:3000", "http://34.207.245.32:4000")
                         .exposedHeaders("x-total-count");
 
             }
